426fdaea82cd59e7486ec35b46d58971709b81c4
Mathias <mathiassolar24@gmail.com>
2024-07-03 18:07:08 -0400

    login

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/pages/home_page.dart';
+import 'package:pokedex/pages/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LoginScreen(),
     );
   }
 }
