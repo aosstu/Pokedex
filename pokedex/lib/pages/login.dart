04af685e6204ae614bfdd15be4ddfb31854f9cf5
Mathias <mathiassolar24@gmail.com>
2024-07-03 19:09:53 -0400

    login terminado

--- pokedex/lib/pages/login.dart
+++ pokedex/lib/pages/login.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api, unnecessary_null_comparison
-import 'dart:ui';
-import 'package:firebase_storage/firebase_storage.dart';
+// ignore_for_file: unnecessary_null_comparison
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pokedex/pages/home_page.dart';
 
@@ -18,11 +17,10 @@ class _LoginScreenState extends State<LoginScreen> {
   late String password;
   String errorMessage = '';
   String _imageUrl = '';
+
   Future<void> _searchImage(String searchText) async {
-    // Normalizar el texto de entrada
     final normalizedText = searchText.trim();
 
-    // Buscar la imagen en Firebase Storage
     final storageRef =
         FirebaseStorage.instance.ref().child('fondo/$normalizedText.jpg');
 
@@ -41,121 +39,145 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     _searchImage('fondologin');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(image: NetworkImage(_imageUrl))),
-        child: Center(
-          child: Container(
-            margin: const EdgeInsets.all(24.0),
-            padding: const EdgeInsets.all(24.0),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Bienvenido a la ',
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Text(
-                    'Pokedex',
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  const Text(
-                    'Inicia sesión para continuar',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (value) {
-                      email = value;
-                    },
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.email),
-                      hintText: 'Correo electrónico',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    obscureText: true,
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      hintText: 'Contraseña',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                  if (errorMessage.isNotEmpty)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text(
-                        errorMessage,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                    ),
-                  const SizedBox(height: 24.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      backgroundColor: Colors.red,
-                      textStyle: const TextStyle(fontSize: 18.0),
-                      minimumSize: const Size(double.infinity, 50),
-                    ),
-                    onPressed: () async {
-                      try {
-                        final newUser = await _auth.signInWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        if (newUser != null) {
-                          setState(() {
-                            errorMessage = '';
-                          });
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()),
-                          );
-                        }
-                      } catch (e) {
-                        setState(() {
-                          errorMessage =
-                              'El usuario o contraseña no son correctos';
-                        });
-                      }
-                    },
-                    child: const Text(
-                      'Iniciar sesión',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          _buildBackgroundImage(),
+          _buildLoginForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: _imageUrl.isNotEmpty
+            ? DecorationImage(
+                image: NetworkImage(_imageUrl),
+                fit: BoxFit.cover,
+              )
+            : null, 
+      ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Bienvenido a la ',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
+              const Text(
+                'Pokedex',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Inicia sesión para continuar',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.email),
+                  hintText: 'Correo electrónico',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock),
+                  hintText: 'Contraseña',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              if (errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ),
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  backgroundColor: Colors.red,
+                  textStyle: const TextStyle(fontSize: 18.0),
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                onPressed: () async {
+                  try {
+                    final newUser = await _auth.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    if (newUser != null) {
+                      setState(() {
+                        errorMessage = '';
+                      });
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomePage(),
+                        ),
+                      );
+                    }
+                  } catch (e) {
+                    setState(() {
+                      errorMessage = 'El usuario o contraseña no son correctos';
+                    });
+                  }
+                },
+                child: const Text(
+                  'Iniciar sesión',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
         ),
       ),
