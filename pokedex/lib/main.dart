a7644e3aaeb759edef6caec0036a5c91acf5c866
Mathias <mathiassolar24@gmail.com>
2024-07-03 22:49:29 -0400

    optimizacion codigo

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex/pages/home_page.dart';
 import 'package:pokedex/pages/login.dart';
 
 void main() async {
