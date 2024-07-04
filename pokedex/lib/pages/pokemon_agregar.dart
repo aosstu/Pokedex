caea7e64f25c02b98972f4804136160a42f384da
CarlitoxDG <carlitoxx102@gmail.com>
2024-07-03 21:15:43 -0400

    inicio agregar pokemon

---
+++ pokedex/lib/pages/pokemon_agregar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgregarPokemon extends StatefulWidget {
+  const AgregarPokemon({super.key});
+
+  @override
+  State<AgregarPokemon> createState() => _AgregarPokemonState();
+}
+
+class _AgregarPokemonState extends State<AgregarPokemon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- pokedex/lib/pages/tabs/tab_pokedex.dart
+++ pokedex/lib/pages/tabs/tab_pokedex.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:pokedex/pages/pokemon_agregar.dart';
 import 'package:pokedex/services/firestore_service.dart';
 import 'package:pokedex/widgets/pokemon_list.dart';
 
@@ -29,6 +31,24 @@ class _TabPokedexState extends State<TabPokedex> {
             }
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          MdiIcons.plus,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        onPressed: () {
+          MaterialPageRoute ruta = MaterialPageRoute(
+            builder: (context) => AgregarPokemon(),
+          );
+          Navigator.push(context, ruta).then(
+            (value) {
+              setState(() {});
+            },
+          );
+        },
       ),
     );
   }
